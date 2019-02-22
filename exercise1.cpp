5d8fdac10187ae45f4107564179d6340bc768dc8
Ian Becker <39637301+Ianbcode@users.noreply.github.com>
2019-02-21 17:44:44 -0800

    Deleted file
    moved exercise file into lab 1 already.

--- exercise1.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>		//Accessing system libary iostream.
-
-using namespace std;		//Abillity to use cin, cout, etc.
-
-int main() {		//Main body
-	cout << "Welcome to computer science!/n" << endl;		//prints char in "". 
-	return 0;		//Return 0 if program works
-}
