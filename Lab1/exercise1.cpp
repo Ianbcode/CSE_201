102a6978e3c1021b11c3e9b1574a411d2e2f6317
Ian Becker <39637301+Ianbcode@users.noreply.github.com>
2019-02-21 17:35:38 -0800

    Added file
    lab 1 first exercise

---
+++ Lab1/exercise1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>		//Accessing system libary iostream.
+
+using namespace std;		//Abillity to use cin, cout, etc.
+
+int main() {		//Main body
+	cout << "Welcome to computer science!/n" << endl;		//prints char in "". 
+	return 0;		//Return 0 if program works
+}
